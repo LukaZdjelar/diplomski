8fa04a157213dddf6775e96d81658aa0c5ad9b34
Luka Zdjelar <luka.zdjelar7@gmail.com>
2022-11-07 16:48:56 +0100

    Introduce two way binding with database with Flow parte dos

---
+++ diplomski-android/app/src/main/java/com/example/diplomski_android/ui/fragment/LessonsFragment.kt
@@ -0,0 +1,60 @@
+package com.example.diplomski_android.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.diplomski_android.MainViewModel
+import com.example.diplomski_android.R
+import com.example.diplomski_android.databinding.FragmentLessonsBinding
+import com.example.diplomski_android.databinding.FragmentTaskBinding
+import com.example.diplomski_android.ui.adapter.ChaptersAdapter
+import com.example.diplomski_android.ui.adapter.LessonsAdapter
+import kotlinx.android.synthetic.main.fragment_chapters.*
+import kotlinx.android.synthetic.main.fragment_lessons.*
+import kotlinx.coroutines.flow.collectLatest
+
+class LessonsFragment : Fragment() {
+    private val mainViewModel : MainViewModel by activityViewModels()
+    private var lessonsBinding : FragmentLessonsBinding? = null
+    private lateinit var lessonsAdapter: LessonsAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val fragmentBinding = FragmentLessonsBinding.inflate(inflater, container, false)
+        lessonsBinding = fragmentBinding
+
+        return fragmentBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lessonsBinding?.apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewModel = mainViewModel
+            lessonsFragment = this@LessonsFragment
+        }
+
+        setupRecyclerView()
+        lifecycleScope.launchWhenCreated {
+            mainViewModel.lessonsStateFlow.collectLatest {
+                lessonsAdapter.differ.submitList(it)
+            }
+        }
+    }
+
+    private fun setupRecyclerView(){
+        lessonsAdapter = LessonsAdapter(mainViewModel)
+        rvLessons.apply {
+            adapter = lessonsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+}
